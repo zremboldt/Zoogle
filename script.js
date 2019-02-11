e3a662445a795281f8ff653a8de716de8d39a732
zacaree <zacharem@gmail.com>
2019-02-10 21:17:00 -0600

    Getting started on the logo color shuffle.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let app = Array.from(document.querySelectorAll(".app"));
+// Footer animate on
+const app = Array.from(document.querySelectorAll(".app"));
 
-// Animation
 app.forEach((app, i) => {
   const setActive = () => app.classList.add("active");
   const runSequence = () => setTimeout(setActive, 40 * i);
   setTimeout(runSequence, 500);
 });
+
+// Logo color mixup on keyup
+const input = document.querySelector(".searchInput");
+
+input.addEventListener("keyup", () => console.log("keyupped"));
