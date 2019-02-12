b90f932d1de7899d393820ea0c456a886e9a69a8
zacaree <zacharem@gmail.com>
2019-02-11 21:23:49 -0600

    Color cycle on keypress feature complete!

--- script.js
+++ script.js
@@ -7,18 +7,26 @@ app.forEach((app, i) => {
   setTimeout(runSequence, 500);
 });
 
-// Logo color mixup on keyup
+// Logo color mixup on keydown
 const input = document.querySelector(".searchInput");
 const letters = Array.from(document.querySelectorAll(".letter"));
 
-const colorize = () => {
+const cycleColor = () => {
   letters.forEach(letter => {
-    // class list includes red ? change to blue
-    // class list includes blue ? change to green
-    // class list includes green ? change to yellow
-    // class list includes yellow ? change to red
+    if (letter.classList.contains("red")) {
+      letter.classList.remove("red");
+      letter.classList.add("blue");
+    } else if (letter.classList.contains("blue")) {
+      letter.classList.remove("blue");
+      letter.classList.add("green");
+    } else if (letter.classList.contains("green")) {
+      letter.classList.remove("green");
+      letter.classList.add("yellow");
+    } else {
+      letter.classList.remove("yellow");
+      letter.classList.add("red");
+    }
   });
 };
 
-input.addEventListener("keyup", () => console.log("keyupped"));
-console.log(letters);
+input.addEventListener("keydown", () => cycleColor());
